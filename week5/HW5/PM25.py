ba416c68708ae9d0a56209439c621e4aef929aed
Natthapol-PEET <natthapol.n@ku.th>
2020-08-23 20:08:34 +0700

    HW-5

--- week5/HW5/PM25.py
+++ week5/HW5/PM25.py
@@ -24,28 +24,41 @@ data_formatted_str = json.dumps(data_dict, indent=2)
 # print( data_dict['location'] )
 # print( data_dict['forecasts'] )
 
+# hour = [(data_dict['forecasts'][i]['date'].split('T'))[1][:5] for i in range(6)]
+# values = [(data_dict['forecasts'][i]['values'][0]['value']) for i in range(6)]
+
+# print(hour)
+# print(values)
+
+timeZone = data_dict['location']['timeZone']
+date = ((data_dict['forecasts'][0]['date']).split('T'))[0]
+time = ((data_dict['forecasts'][0]['date']).split('T'))[1][:5]
+pm25 = (data_dict['forecasts'][0]['values'][0]['value'])
+
+date_set = [((data_dict['forecasts'][i]['date']).split('T'))[0] for i in range(len(data_dict['forecasts']))]
+date_set = list(dict.fromkeys(date_set))
+data_val = 0
 count = 0
+pm25_avg = []
 
-reg_date = 'None'
+for x in date_set:
+    for y in data_dict['forecasts']:
+        if x in y['date']:
+            data_val += y['values'][0]['value']
+            count += 1
+    data_val = round(data_val / count, 2)
+    if data_val < 25:
+        pm25_avg.append([0, x, data_val])
+    elif data_val < 37:
+        pm25_avg.append([1, x, data_val])
+    elif data_val < 50:
+        pm25_avg.append([2, x, data_val])
+    elif data_val < 90:
+        pm25_avg.append([3, x, data_val])
+    else:
+        pm25_avg.append([5, x, data_val])
+    data_val = 0
+    count = 0
 
-for i in range(len(data_dict['forecasts'])):
-    # print( data_dict['forecasts'][i] )
-    # if '2020-08-19' in str(data_dict['forecasts'][i]):
-    #     print( data_dict['forecasts'][i] )
-
-    #     count += 1
-
-    date = data_dict['forecasts'][i]['date']
-    date = date.split('T')
-
-    # if reg_date != date[0]:
-    #     reg_date = date[0]
-    #     print( date[0] )
-
-# print( count )
-
-hour = [(data_dict['forecasts'][i]['date'].split('T'))[1][:5] for i in range(6)]
-values = [(data_dict['forecasts'][i]['values'][0]['value']) for i in range(6)]
-
-print(hour)
-print(values)
+for x in pm25_avg:
+    print(x)
--- week5/HW5/app.py
+++ week5/HW5/app.py
@@ -22,9 +22,40 @@ data_formatted_str = json.dumps(data_dict, indent=2)
 hour = [(data_dict['forecasts'][i+1]['date'].split('T'))[1][:5] for i in range(6)]
 values = [(data_dict['forecasts'][i+1]['values'][0]['value']) for i in range(6)]
 
+timeZone = data_dict['location']['timeZone']
+date = ((data_dict['forecasts'][0]['date']).split('T'))[0]
+time = ((data_dict['forecasts'][0]['date']).split('T'))[1][:5]
+pm25 = (data_dict['forecasts'][0]['values'][0]['value'])
+data = [date, time, timeZone, pm25]
+
+date_set = [((data_dict['forecasts'][i]['date']).split('T'))[0] for i in range(len(data_dict['forecasts']))]
+date_set = list(dict.fromkeys(date_set))
+data_val = 0
+count = 0
+pm25_avg = []
+
+for x in date_set:
+    for y in data_dict['forecasts']:
+        if x in y['date']:
+            data_val += y['values'][0]['value']
+            count += 1
+    data_val = round(data_val / count, 2)
+    if data_val < 25:
+        pm25_avg.append([0, x, data_val])
+    elif data_val < 37:
+        pm25_avg.append([1, x, data_val])
+    elif data_val < 50:
+        pm25_avg.append([2, x, data_val])
+    elif data_val < 90:
+        pm25_avg.append([3, x, data_val])
+    else:
+        pm25_avg.append([4, x, data_val])
+    data_val = 0
+    count = 0
+
 @app.route('/')
 def index():
-    return render_template('index.html', hour=hour, values=values)
+    return render_template('index.html', hour=hour, values=values, data=data, pm25_avg=pm25_avg)
 
 if __name__ == "__main__":
     app.run(debug=True)
